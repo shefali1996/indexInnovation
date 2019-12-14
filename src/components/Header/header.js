277e0aa87f42c4a7def4e8f34340a72d80dce899
reactexcel <arun.etech2011@gmail.com>
2019-12-14 17:48:13 +0530

    a/b testing pending

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,50 +2,63 @@ import React, { Component } from "react";
 import ReactFlagsSelect from "react-flags-select";
 import logoImg from "../../assets/images/logo.png";
 import { I18n, setLocale } from "react-redux-i18n";
-import { Link,withRouter } from "react-router";
+import { Link, withRouter } from "react-router";
 import "./header.scss";
 import "react-flags-select/css/react-flags-select.css";
 import "react-flags-select/scss/react-flags-select.scss";
 import { connect } from "react-redux";
-import ModalForm from '../ModalForm/ModalForm'
-
+import ModalForm from "../ModalForm/ModalForm";
+import {
+  Experiment,
+  Variant,
+  emitter,
+  experimentDebugger
+} from "@marvelapp/react-ab-test";
+experimentDebugger.enable();
+emitter.defineVariants('Test-Addl-Info',['tryFree','expIDEX','getStarted'],  [33,33,33])
 class Header extends Component {
   state = {
     mobileHeader: false,
-    show:false
+    show: false
   };
   handleClick = () => {
     this.setState(prevState => ({
       mobileHeader: !prevState.mobileHeader
     }));
   };
+  onclick (e) {
+    console.log(emitter.getActiveVariant("Test-Addl-Info'"),'dddddddddddddd')
+    emitter.emitWin('Test-Addl-Info')
 
-  handelShowModal=()=>{    
+  }
+  handelShowModal = () => {
+    this.onclick()
     this.setState(prevState => ({
       show: true
-    }))
-  }
-  handleLogoClick=()=>{
-    if(window.location.pathname==="/"){
-      window.scrollTo(0,0)
+    }));
+  };
+  handleLogoClick = () => {
+    if (window.location.pathname === "/") {
+      window.scrollTo(0, 0);
+    } else {
+      this.props.router.push("/");
     }
-    else{
-      this.props.router.push("/")      
-    }
-  }
+  };
   render() {
     const { mobileHeader } = this.state;
+    console.log(emitter.getActiveVariant("Test-Addl-Info'"),'dddddddddddddd')
     return (
       <>
-      <ModalForm show={this.state.show}
-      handleCloseModal={()=>{
-        this.setState({
-          show:false
-        })
-      }}
-      />
+        <ModalForm
+          show={this.state.show}
+          handleCloseModal={() => {
+            this.setState({
+              show: false
+            });
+          }}
+        />
         <div className={(!mobileHeader && "hide-header ") + " header-wrapper"}>
-          <div className="header-logo-wrapper" >
+          <div className="header-logo-wrapper">
             <i
               className="fa fa-times-thin fa-2x"
               aria-hidden="true"
@@ -64,7 +77,7 @@ class Header extends Component {
               >
                 {I18n.t("header.home")}
               </Link>
-             
+
               <Link
                 to="/blog"
                 className={
@@ -94,24 +107,40 @@ class Header extends Component {
                 }}
               />
             </div>
-            <div className="trial-btn" onClick={this.handelShowModal}>{I18n.t("header.tryFree")}</div>
+            <Experiment ref="ab-more-info" name="Test-Addl-Info">
+              <Variant name="tryFree">
+                <div className="trial-btn" onClick={this.handelShowModal}>
+                  {I18n.t("header.tryFree")}
+                </div>
+              </Variant>
+              <Variant name="expIDEX">
+                <div className="trial-btn" onClick={this.handelShowModal}>
+                  {I18n.t("headSection.expIDEX")}
+                </div>
+              </Variant>
+              <Variant name="getStarted">
+                <div className="trial-btn" onClick={this.handelShowModal}>
+                  {I18n.t("footer.getStarted")}
+                </div>
+              </Variant>
+            </Experiment>
           </div>
         </div>
-          <div
-            className={
-              (mobileHeader && "hide-header ") + " mobile-small-header"
-            }
-          >
-            <div className="header-logo-wrapper">
-              <i
-                class="fa fa-bars"
-                aria-hidden="true"
-                onClick={this.handleClick}
-              ></i>
-              <img src={logoImg} alt="IDEX Logo" />
-            </div>
-            <div className="trial-btn-mobile" onClick={this.handelShowModal}>{I18n.t("header.tryFree")}</div>
+        <div
+          className={(mobileHeader && "hide-header ") + " mobile-small-header"}
+        >
+          <div className="header-logo-wrapper">
+            <i
+              class="fa fa-bars"
+              aria-hidden="true"
+              onClick={this.handleClick}
+            ></i>
+            <img src={logoImg} alt="IDEX Logo" />
           </div>
+          <div className="trial-btn-mobile" onClick={this.handelShowModal}>
+            {I18n.t("header.tryFree")}
+          </div>
+        </div>
       </>
     );
   }
@@ -123,3 +152,14 @@ function mapStateToProps(state) {
   };
 }
 export default withRouter(connect(mapStateToProps)(Header));
+
+// We want to add a 'play listener to record every instance a user sees a variant.
+emitter.addPlayListener("Test-Addl-Info", function(experimentName, variantName) {
+  console.log(`Displaying experiment ${experimentName} variant   ${variantName}`);
+  // Perform any necessary operations to send experiment data to server or analytics provider.
+  });
+  // The win listener is only called when the Win condition is met, in this instance, when the Learn More button is pressed.
+  emitter.addWinListener("Test-Addl-Info", function(experimentName, variantName) {
+    console.log(`Variant ${variantName} of experiment ${experimentName} was clicked`);
+    // Perform any necessary operations to send experiment data to server or analytics provider.
+  });
