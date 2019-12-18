69f4515971adfa4190f6f11c3a9ce7f06ab76f31
reactexcel <arun.etech2011@gmail.com>
2019-12-18 11:44:22 +0530

    home page logo done

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,63 +1,72 @@
-import React, { Component } from 'react'
-import { I18n } from 'react-redux-i18n'
-import ContactForm from '../ContactForm/ContactForm'
-import './Resources.scss' 
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import { I18n } from "react-redux-i18n";
+import ContactForm from "../ContactForm/ContactForm";
+import "./Resources.scss";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import toArray from "lodash/toArray";
+import isEqual from "lodash/isEqual";
+import ReactHtmlParser from "react-html-parser";
 
 class Resources extends Component {
-  state = {isError:false}
-  sendDataToChild=(data)=>{
-this.setState({
-  isError:data
-})
+  state = { isError: false, blog: toArray(I18n.t("blog").articles).reverse() };
+  sendDataToChild = data => {
+    this.setState({
+      isError: data
+    });
+  };
+  componentDidUpdate(props) {
+    if (!isEqual(this.state.blog, toArray(I18n.t("blog").articles).reverse())) {
+      this.setState({
+        blog: toArray(I18n.t("blog").articles).reverse()
+      });
+    }
   }
-  render() {    
+  handleSeeAllClick = () => {
+    this.props.history.push("/blog");
+  };
+
+  handleTitleClick=(route)=>{
+    let blogRoute = route.split(" ").join("_");
+    this.props.history.push(`/blog/${blogRoute}`);
+  }
+  render() {
     return (
-      <div className={"resource-wrapper " +(this.state.isError && "resource-wrapper-error")}>
-        <h2>{I18n.t('resource.title')}</h2>
-        <p>{I18n.t('resource.learn')}</p>
+      <div
+        className={
+          "resource-wrapper " + (this.state.isError && "resource-wrapper-error")
+        }
+      >
+        <h2>{I18n.t("resource.title")}</h2>
+        <p>{I18n.t("resource.learn")}</p>
         <div className="trusty-articles">
-          <div className="trusty-articles-item">
-            <div className="article-head">
-              <span>22 Oct 2019 | {I18n.t('resource.by')} </span>
-              <span className="article-author">Zijin</span>
+          {this.state.blog.slice(0, 3).map(val => (
+            <div className="trusty-articles-item">
+              <div className="article-head">
+                <span>
+                  {val.date} | {I18n.t("resource.by")}{" "}
+                </span>
+                <span className="article-author">{val.by}</span>
+              </div>
+              <p className="article-title" onClick={()=>this.handleTitleClick(val.route)}>{val.title}</p>
+              <div className="article-content">
+                {ReactHtmlParser(val.excerpt)}
+              </div>
             </div>
-            <p className="article-title">10 Steps from Idea to Profit</p>
-            <div className="article-content">Tmall and Taobao have delivered the largest-ever – in both scale and reach – 6.18 Mid-Year Shopping Festival for Alibaba Group, connecting a record number of brands with the largest source of untapped consumption growth in China: the country’s hundreds of still-developing cities.
-
-            Over 200,000 brands participated in 6.18 this year, said Alibaba. Apart from offering discounts and perks in their Tmall and Taobao stores, brands tapped Juhuasuan, Alibaba’s dedicated flash-sale and group-buying platform, to introduce deals exclusive to the festival. During 6.18, Juhuasuan saw 4,700 featured products top RMB 1 million ($145,000) in sales. There were 180 products that each took in over RMB 10 million on the platform, according to Juhuasuan.</div>
-          </div>
-          <div className="trusty-articles-item">
-            <div className="article-head">
-              <span>22 Oct 2019 | {I18n.t('resource.by')} </span>
-              <span className="article-author">Zijin</span>
-            </div>
-            <p className="article-title">10 Steps from Idea to Profit</p>
-            <div className="article-content">Tmall and Taobao have delivered the largest-ever – in both scale and reach – 6.18 Mid-Year Shopping Festival for Alibaba Group, connecting a record number of brands with the largest source of untapped consumption growth in China: the country’s hundreds of still-developing cities.
-
-            Over 200,000 brands participated in 6.18 this year, said Alibaba. Apart from offering discounts and perks in their Tmall and Taobao stores, brands tapped Juhuasuan, Alibaba’s dedicated flash-sale and group-buying platform, to introduce deals exclusive to the festival. During 6.18, Juhuasuan saw 4,700 featured products top RMB 1 million ($145,000) in sales. There were 180 products that each took in over RMB 10 million on the platform, according to Juhuasuan.</div>
-          </div>
-          <div className="trusty-articles-item">
-            <div className="article-head">
-              <span>22 Oct 2019 | {I18n.t('resource.by')} </span>
-              <span className="article-author">Zijin</span>
-            </div>
-            <p className="article-title">10 Steps from Idea to Profit</p>
-            <div className="article-content">Tmall and Taobao have delivered the largest-ever – in both scale and reach – 6.18 Mid-Year Shopping Festival for Alibaba Group, connecting a record number of brands with the largest source of untapped consumption growth in China: the country’s hundreds of still-developing cities.
-
-            Over 200,000 brands participated in 6.18 this year, said Alibaba. Apart from offering discounts and perks in their Tmall and Taobao stores, brands tapped Juhuasuan, Alibaba’s dedicated flash-sale and group-buying platform, to introduce deals exclusive to the festival. During 6.18, Juhuasuan saw 4,700 featured products top RMB 1 million ($145,000) in sales. There were 180 products that each took in over RMB 10 million on the platform, according to Juhuasuan.</div>
-          </div>
+          ))}
         </div>
-        <div className="all-article-btn">{I18n.t('resource.articles')}</div>
-        <ContactForm sendDataToChild={this.sendDataToChild}/>
+        <div className="all-article-btn" onClick={this.handleSeeAllClick}>
+          {I18n.t("resource.articles")}
+        </div>
+        <ContactForm sendDataToChild={this.sendDataToChild} />
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    i18n : state.i18n
-  }
+    i18n: state.i18n
+  };
 }
-export default connect(mapStateToProps)(Resources);
+export default withRouter(connect(mapStateToProps)(Resources));
